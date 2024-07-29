94ef7cc4bcb53349f0234cb468751380bda34355
khuzzzi <khuzema123kashif@gmail.com>
2024-07-29 06:52:46 +0100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.route.js"
+
+
 
 dotenv.config()
 
@@ -8,3 +11,6 @@ mongoose.connect(process.env.MONGO_URL).then(()=>console.log("mongodb connected"
 const app = express();
 
 app.listen(3000,()=>console.log(`Server started running at server 3000`))
+
+
+app.use("/api/users",userRoutes)
