aefc4918a83ea56c993c3d469b15c1cb5081d049
khuzzzi <khuzema123kashif@gmail.com>
2024-07-29 06:32:11 +0100

    database connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log("mongodb connected"))
 const app = express();
 
 app.listen(3000,()=>console.log(`Server started running at server 3000`))
